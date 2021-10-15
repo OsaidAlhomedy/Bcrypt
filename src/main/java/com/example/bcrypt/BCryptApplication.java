86b66bbefcd0c5f2d0aeec9886b4bfe2c91282d6
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-10-15 03:41:42 +0300

    initialization

---
+++ src/main/java/com/example/bcrypt/BCryptApplication.java
@@ -0,0 +1,13 @@
+package com.example.bcrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BCryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BCryptApplication.class, args);
+    }
+
+}
